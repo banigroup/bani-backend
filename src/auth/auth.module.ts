d8d2dfea7dde8baa91f535900a0f719260895fb0
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 17:34:26 +0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,1 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { OtpService } from './otp/otp.service';
+import { TokenService } from './tokens/token.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { SMS_PROVIDER } from './otp/sms/sms-provider.interface';
+import { ConsoleSmsProvider } from './otp/sms/console-sms.provider';
 
+@Module({
+  imports: [PassportModule, JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    OtpService,
+    TokenService,
+    JwtStrategy,
+    { provide: SMS_PROVIDER, useClass: ConsoleSmsProvider },
+  ],
+  exports: [AuthService],
+})
+export class AuthModule {}
