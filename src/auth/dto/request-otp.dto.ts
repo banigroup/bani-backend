d9dd6c9c3c5c1d483f95d3747e12d833d00f0a52
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 17:22:59 +0300

    Update request-otp.dto.ts

--- src/auth/dto/request-otp.dto.ts
+++ src/auth/dto/request-otp.dto.ts
@@ -1,1 +1,7 @@
+import { IsString, Matches } from 'class-validator';
 
+export class RequestOtpDto {
+  @IsString()
+  @Matches(/^\+[1-9]\d{9,14}$/, { message: 'Telefon E.164 formatında olmalı' })
+  phone!: string;
+}
