279de84c23b6dc24a61b3b66075b4bdf94a29911
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 16:45:40 +0300

    Update permissions.guard.ts

--- src/common/rbac/permissions.guard.ts
+++ src/common/rbac/permissions.guard.ts
@@ -1,1 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Permission } from './permissions.enum';
+import { PERMISSIONS_KEY } from './permissions.decorator';
+import { permissionsForRoles } from './role-permissions';
 
+@Injectable()
+export class PermissionsGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const required = this.reflector.getAllAndOverride<Permission[]>(PERMISSIONS_KEY, [ctx.getHandler(), ctx.getClass()]);
+    if (!required || required.length === 0) return true;
+    const { user } = ctx.switchToHttp().getRequest();
+    const granted = permissionsForRoles(user?.roles ?? []);
+    if (!required.every((p) => granted.has(p))) throw new ForbiddenException('Yetersiz izin');
+    return true;
+  }
+}
