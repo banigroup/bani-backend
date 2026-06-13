f77074d07be256a4c1cf86ac4b7646dd072bb8b1
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 16:44:18 +0300

    Update roles.guard.ts

--- src/common/rbac/roles.guard.ts
+++ src/common/rbac/roles.guard.ts
@@ -1,1 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from '@prisma/client';
+import { ROLES_KEY } from './roles.decorator';
 
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(ctx: ExecutionContext): boolean {
+    const required = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [ctx.getHandler(), ctx.getClass()]);
+    if (!required || required.length === 0) return true;
+    const { user } = ctx.switchToHttp().getRequest();
+    const roles: Role[] = user?.roles ?? [];
+    if (!required.some((r) => roles.includes(r))) throw new ForbiddenException('Yetersiz rol');
+    return true;
+  }
+}
