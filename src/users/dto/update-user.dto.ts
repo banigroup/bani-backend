e6c99c9cbd0e8d0279fd9b1b47e86af00831665c
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 17:51:10 +0300

    Update update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,1 +1,7 @@
+import { IsEmail, IsOptional, IsString, MaxLength } from 'class-validator';
 
+export class UpdateUserDto {
+  @IsOptional() @IsString() @MaxLength(80) name?: string;
+  @IsOptional() @IsString() @MaxLength(80) surname?: string;
+  @IsOptional() @IsEmail() email?: string;
+}
