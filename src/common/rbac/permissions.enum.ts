460134bd47994a939f9b7d7d892b9341e5a15d1b
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 16:40:14 +0300

    Update permissions.enum.ts

--- src/common/rbac/permissions.enum.ts
+++ src/common/rbac/permissions.enum.ts
@@ -1,1 +1,14 @@
-
+export enum Permission {
+  USER_READ = 'user:read',
+  USER_WRITE = 'user:write',
+  USER_ROLE_ASSIGN = 'user:role:assign',
+  USER_SUSPEND = 'user:suspend',
+  ADDRESS_READ = 'address:read',
+  ADDRESS_WRITE = 'address:write',
+  WALLET_READ = 'wallet:read',
+  WALLET_TOPUP = 'wallet:topup',
+  WALLET_WITHDRAW = 'wallet:withdraw',
+  TRANSACTION_READ = 'transaction:read',
+  TRANSACTION_REVERSE = 'transaction:reverse',
+  AUDIT_READ = 'audit:read',
+}
