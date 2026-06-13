294220d78070b5d63935f18d931e36fec4dae67f
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 17:26:32 +0300

    Update console-sms.provider.ts

--- src/auth/otp/sms/console-sms.provider.ts
+++ src/auth/otp/sms/console-sms.provider.ts
@@ -1,1 +1,10 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { SmsProvider } from './sms-provider.interface';
 
+@Injectable()
+export class ConsoleSmsProvider implements SmsProvider {
+  private readonly logger = new Logger('SMS');
+  async send(phone: string, message: string): Promise<void> {
+    this.logger.warn(`[DEV SMS] -> ${phone}: ${message}`);
+  }
+}
