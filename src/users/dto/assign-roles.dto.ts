728401b3a98875b69326772e157d8620d3f8c439
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 17:52:32 +0300

    Update assign-roles.dto.ts

--- src/users/dto/assign-roles.dto.ts
+++ src/users/dto/assign-roles.dto.ts
@@ -1,1 +1,9 @@
+import { IsArray, ArrayNotEmpty, IsEnum } from 'class-validator';
+import { Role } from '@prisma/client';
 
+export class AssignRolesDto {
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsEnum(Role, { each: true })
+  roles!: Role[];
+}
