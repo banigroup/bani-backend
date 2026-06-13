3227c2640552773879638de9d1a62b197125d8f5
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-06-13 16:51:14 +0300

    Update current-user.decorator.ts

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,1 +1,15 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Role } from '@prisma/client';
 
+export interface AuthUser {
+  id: string;
+  phone: string;
+  roles: Role[];
+}
+
+export const CurrentUser = createParamDecorator(
+  (data: keyof AuthUser | undefined, ctx: ExecutionContext): AuthUser | unknown => {
+    const req = ctx.switchToHttp().getRequest();
+    return data ? req.user?.[data] : req.user;
+  },
+);
