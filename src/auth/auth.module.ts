ec654a079fd7bfa6f0ed3631a8085c4aae9142ae
BANİGROUP <zrr8k7y86h@privaterelay.appleid.com>
2026-07-12 19:30:17 +0300

    chore: SMS provider teshis logu (SMS_AKTIF, SENDER, KEY gorunur)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from "@nestjs/common";
+﻿import { Module, Logger } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
 import { AuthController } from "./auth.controller";
@@ -9,6 +9,8 @@ import { JwtStrategy } from "./strategies/jwt.strategy";
 import { SMS_PROVIDER } from "./otp/sms/sms-provider.interface";
 import { ConsoleSmsProvider } from "./otp/sms/console-sms.provider";
 import { IletiMerkeziSmsProvider } from "./otp/sms/iletimerkezi-sms.provider";
+const smsAktif = process.env.SMS_AKTIF === "true";
+new Logger("AuthModule").log(`SMS provider: ${smsAktif ? "ILETI MERKEZI" : "CONSOLE"} | SMS_AKTIF=[${process.env.SMS_AKTIF}] | SENDER=[${process.env.ILETIMERKEZI_SENDER}] | KEY_VAR=[${!!process.env.ILETIMERKEZI_KEY}]`);
 @Module({
   imports: [PassportModule, JwtModule.register({})],
   controllers: [AuthController],
